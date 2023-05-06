a98062bc6082df8bebd16b772084f42e02ec82e9
GitBolt <saabisakhtar@gmail.com>
2023-05-06 12:14:14 +0530

    fix: data fetching and state updates for editor

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -17,7 +17,7 @@ import { AddAccess } from "@/components/AddAccess"
 import { useRouter } from "next/router"
 import { NewButton } from "@/components/NewButton"
 
-export const Navbar = ({ multiplayer = false, editAccess = false }: { multiplayer?: boolean, editAccess?: boolean }) => {
+export const Navbar = ({ multiplayer = false, editAccess = false, setEditAccess }: { multiplayer?: boolean, editAccess?: boolean, setEditAccess?: any }) => {
 
   const { toObject } = useReactFlow()
 
@@ -35,13 +35,40 @@ export const Navbar = ({ multiplayer = false, editAccess = false }: { multiplaye
     setName(currentPlayground.name)
   }, [currentPlayground])
 
+
   useEffect(() => {
 
     const run = async () => {
+
+
       if (multiplayer) {
         const res = await fetch(`/api/playground/get/id/${router.query.id}`)
-        const data = await res.json()
-        setCurrentPlayground(data)
+        const pgData = await res.json()
+
+        if (publicKey) {
+          const userReq = await fetch(`/api/user/${publicKey.toBase58()}`)
+          let userData = await userReq.json()
+          if (!userReq.ok) {
+            const newUserReq = await fetch(`/api/user/new`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ publicKey }),
+            })
+            userData = await newUserReq.json()
+          }
+
+          setUser(userData)
+          setCurrentPlayground(pgData)
+          setEditAccess(
+            userData.id == pgData.userId ||
+            pgData.edit_access.includes(publicKey.toBase58())
+          )
+        } else {
+          setUser(null)
+          setEditAccess(false)
+        }
       } else {
         const res = await fetch(`/api/playground/get/${publicKey?.toBase58()}`)
         const data = await res.json()
@@ -49,32 +76,9 @@ export const Navbar = ({ multiplayer = false, editAccess = false }: { multiplaye
       }
     }
     run()
-  }, [multiplayer, router.query.id, publicKey])
+  }, [multiplayer, router.query.id, publicKey, setEditAccess])
 
 
-  useEffect(() => {
-    if (!publicKey) {
-      setUser(null)
-      return
-    }
-    const run = async () => {
-      const userReq = await fetch(`/api/user/${publicKey.toBase58()}`)
-      let user = await userReq.json()
-      if (!userReq.ok) {
-        const newUserReq = await fetch(`/api/user/new`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ publicKey }),
-        })
-        user = await newUserReq.json()
-
-      }
-      setUser(user)
-    }
-    run()
-  }, [publicKey])
 
 
   const handlePlaygroundSave = async () => {
@@ -155,22 +159,22 @@ export const Navbar = ({ multiplayer = false, editAccess = false }: { multiplaye
       {multiplayer && <AddAccess playground={currentPlayground!} setCurrentPlayground={setCurrentPlayground} />}
 
 
-      {user ?
+      {((multiplayer && user && currentPlayground && user.id == currentPlayground?.userId) || (!multiplayer && user)) &&
         <Flex align="center" gap="1rem">
 
           <Button variant="filled" h="3rem" fontSize="1.7rem" onClick={handlePlaygroundSave}>Save</Button>
           <Button variant="outline" h="3rem" fontSize="1.7rem" onClick={savedPg.onOpen}>Load</Button>
           <NetworkSelector />
         </Flex>
-        : <NewButton
-          setEdges={setEdges}
-          setNodes={setNodes}
-          setViewport={setViewport}
-          setCurrentPlayground={setCurrentPlayground}
-          user={user}
-        />
       }
 
+      {((!user && multiplayer) || (user && currentPlayground && user.id != currentPlayground.userId && multiplayer)) && <NewButton
+        setEdges={setEdges}
+        setNodes={setNodes}
+        setViewport={setViewport}
+        setCurrentPlayground={setCurrentPlayground}
+        user={user}
+      />}
 
       {multiplayer && user && currentPlayground && user.id == currentPlayground.userId ?
         <Input
@@ -207,7 +211,7 @@ export const Navbar = ({ multiplayer = false, editAccess = false }: { multiplaye
         {user && currentPlayground && user.id == currentPlayground.userId &&
           <Button variant="filled" h="3rem" w="12rem" fontSize="1.5rem" onClick={accessModal.onOpen}>Give access</Button>
         }
-        {((multiplayer && user) || (!multiplayer && user) || (!multiplayer && !user)) && (
+        {((multiplayer && user && currentPlayground && user.id == currentPlayground.userId) || (!multiplayer && !user)) && (
           <NewButton
             setEdges={setEdges}
             setNodes={setNodes}
--- src/pages/playground/[id].tsx
+++ src/pages/playground/[id].tsx
@@ -14,9 +14,6 @@ import { Flex } from "@chakra-ui/react";
 const socket = io('http://localhost:3001');
 
 const Home: NextPage = ({ playground }: any) => {
-  const { publicKey } = useWallet()
-
-  const [user, setUser] = useState(null)
   const [editAccess, setEditAccess] = useState(false)
   const socketRef = useRef(socket)
 
@@ -41,32 +38,10 @@ const Home: NextPage = ({ playground }: any) => {
     onEdgesChange(changes)
   }
 
-  useEffect(() => {
-    if (!publicKey) {
-      setUser(null)
-      setEditAccess(false)
-      return
-    }
-    const run = async () => {
-      const res = await fetch(`/api/user/${publicKey.toBase58()}`)
-      if (res.ok) {
-        const data = await res.json()
-        setUser(data)
-        console.log(data.id, playground.userId)
-        console.log(playground)
-        setEditAccess(
-          data.id == playground.userId ||
-          playground.edit_access.includes(publicKey.toBase58())
-        )
-
-      }
-    }
-    run()
-  }, [publicKey, playground])
 
   useEffect(() => {
+    if (!playground.data) return
     const data = JSON.parse(playground.data)
-    console.log(data)
     setNodes(data.nodes)
     setEdges(data.edges)
   }, [playground, setEdges, setNodes])
@@ -74,7 +49,7 @@ const Home: NextPage = ({ playground }: any) => {
   return (
     <>
       <Flex flexFlow="column" h="100%">
-        <Navbar multiplayer editAccess={editAccess} />
+        <Navbar multiplayer editAccess={editAccess} setEditAccess={setEditAccess} />
         {editAccess && <Sidebar sidebarContent={sidebarContent} />}
         <CommandPalette />
         <Playground
