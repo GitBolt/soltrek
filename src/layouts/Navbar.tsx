3f7d0c620d88060cc120fd4e59958341a28d32c0
GitBolt <saabisakhtar@gmail.com>
2023-05-06 12:49:33 +0530

    fix: more data fetching

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -40,43 +40,42 @@ export const Navbar = ({ multiplayer = false, editAccess = false, setEditAccess 
 
     const run = async () => {
 
+      if (!publicKey) {
+        setUser(null)
+        if (setEditAccess) setEditAccess(null)
+        return
+      }
+      const userReq = await fetch(`/api/user/${publicKey.toBase58()}`)
+      let userData = await userReq.json()
+
+      if (!userReq.ok) {
+        const newUserReq = await fetch(`/api/user/new`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ publicKey }),
+        })
+        userData = await newUserReq.json()
+      }
+
+      setUser(userData)
 
       if (multiplayer) {
         const res = await fetch(`/api/playground/get/id/${router.query.id}`)
         const pgData = await res.json()
-
-        if (publicKey) {
-          const userReq = await fetch(`/api/user/${publicKey.toBase58()}`)
-          let userData = await userReq.json()
-          if (!userReq.ok) {
-            const newUserReq = await fetch(`/api/user/new`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ publicKey }),
-            })
-            userData = await newUserReq.json()
-          }
-
-          setUser(userData)
-          setCurrentPlayground(pgData)
-          setEditAccess(
-            userData.id == pgData.userId ||
-            pgData.edit_access.includes(publicKey.toBase58())
-          )
-        } else {
-          setUser(null)
-          setEditAccess(false)
-        }
-      } else {
-        const res = await fetch(`/api/playground/get/${publicKey?.toBase58()}`)
-        const data = await res.json()
-        setCurrentPlayground(data)
+        setCurrentPlayground(pgData)
+        console.log(pgData, userData)
+        setEditAccess(
+          userData.id == pgData.userId ||
+          pgData.edit_access.includes(publicKey.toBase58())
+        )
       }
     }
+
     run()
-  }, [multiplayer, router.query.id, publicKey, setEditAccess])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [multiplayer, router.query.id, publicKey])
 
 
 
@@ -176,7 +175,7 @@ export const Navbar = ({ multiplayer = false, editAccess = false, setEditAccess 
         user={user}
       />}
 
-      {multiplayer && user && currentPlayground && user.id == currentPlayground.userId ?
+      {multiplayer && user && currentPlayground && user.id == currentPlayground.userId &&
         <Input
           w="22rem"
           h="3rem"
@@ -188,10 +187,11 @@ export const Navbar = ({ multiplayer = false, editAccess = false, setEditAccess 
           value={name}
           defaultValue={name}
           placeholder="Enter Playground Name"
-        /> :
-        <Text color="blue.200" fontWeight={400} fontSize="2.5rem" textAlign="center" >{name}</Text>
-
+        />
       }
+
+      {((multiplayer && !user) || (!multiplayer && !user)) || (multiplayer && user && currentPlayground && user.id != currentPlayground.userId) && <Text color="blue.200" fontWeight={400} fontSize="2.5rem" textAlign="center" >{name}</Text>}
+
       {!multiplayer && user &&
         <Input
           w="22rem"
@@ -211,7 +211,7 @@ export const Navbar = ({ multiplayer = false, editAccess = false, setEditAccess 
         {user && currentPlayground && user.id == currentPlayground.userId &&
           <Button variant="filled" h="3rem" w="12rem" fontSize="1.5rem" onClick={accessModal.onOpen}>Give access</Button>
         }
-        {((multiplayer && user && currentPlayground && user.id == currentPlayground.userId) || (!multiplayer && !user)) && (
+        {((multiplayer && user && currentPlayground && user.id == currentPlayground.userId) || (!multiplayer)) && (
           <NewButton
             setEdges={setEdges}
             setNodes={setNodes}
